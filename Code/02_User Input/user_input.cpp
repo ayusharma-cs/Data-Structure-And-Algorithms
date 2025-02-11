4c201a753455d40be5658fbc7daaaad0b1a2a862
Ayush Sharma <cc.ayusharma@gmail.com>
2025-02-11 19:36:05 +0530

    Basic Code
    Hello World, Data Types, User Input, Type Casting

---
+++ Code/02_User Input/user_input.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+    int a, b;
+    cout << "Enter The Number A: ";
+    cin >> a;
+
+    cout << "Enter The Number B: ";
+    cin >> b;
+
+    cout << "Addition: " << a + b;
+
+    return 0;
+}
